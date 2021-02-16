3e597aa7b8c6503bdafb0d195a3c58285231eda3
ltruong0892 <lhvtruong@gmail.com>
2021-02-16 22:19:45 +0100

    ajout du code complet TP1_PlusMoins

--- TP1_PlusMoins/TP1_PlusMoins/TP1_PlusMoins.cpp
+++ TP1_PlusMoins/TP1_PlusMoins/TP1_PlusMoins.cpp
@@ -4,10 +4,46 @@
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <time.h>
 
 int main()
 {
-    return 0;
+	const int MAX = 100, MIN = 1;
+	int nombreMystere;
+	int nombreDuJoueur = 0, nbrEssais = 0;
+	
+	srand(time(NULL)); // initialise le générateur de nombre aléatoire (à n'executer qu'une seule fois)
+
+	printf("==== Plus ou Moins ====\n");
+	printf("Bonjour et bienvenue dans le jeu Plus ou Moins\n");
+
+	// tirage d'un nombre aléatoire à deviner
+	nombreMystere = (rand() % (MAX - MIN + 1)) + MIN; 
+
+	// Boucle de jeu
+	do
+	{
+		// On demande au joueur d'entrer un nombre
+		printf("Quel est le nombre ? ");
+		scanf("%d", &nombreDuJoueur);
+		nbrEssais++;
+
+		// Boucle de vérification du nombre entré par le joueur
+		if (nombreDuJoueur == nombreMystere)
+		{
+			printf("Bravo, vous avez trouve le nombre mystere\n");
+			printf("Vous avez réussi en %d coups, super !\n", nbrEssais);
+		}
+		else if (nombreDuJoueur < nombreMystere)
+		{
+			printf("C'est plus !\n");
+		}
+		else
+		{
+			printf("C'est moins !\n");
+		}
+	} while (nombreDuJoueur != nombreMystere);
+
+	return 0;
 }
 
